a717e4a889c2ae413c9d049d923c19be8abd038e
Arwa <arwanayef2001@hotmail.com>
2018-03-31 10:44:50 +0300

    home changes

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-	return "hello"
+	return render_template("Home.html")
 
 @app.route("/health")
 def health():
+	return render_template("Health.html")
 
 
 @app.route("/english")
 def english():
+	return render_template("English.html")
 
 
 @app.route("/programming")
 def programming():
-
+	return render_template("Programming.html")
 
 @app.route("/different")
 def different():
-	
+	return render_template("Different.html")	
 
 
 
