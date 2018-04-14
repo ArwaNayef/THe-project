364274cc7bb1ca3ca66d9ed05044c6a12dddebe8
hala <halabarka00@gmail.com>
2018-04-14 10:19:33 +0300

    health

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+
+@app.route("/Health")
+def Health ():
+	return render_template("Health.html")
+
+
+if __name__ =="__main__":
+	app.run(port="9000")
+
+
 #test test 
