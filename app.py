ce34300a0823a4803531910ce42f2b1abdb78004
hala <halabarka00@gmail.com>
2018-04-19 20:23:40 +0300

    massage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 from flask import Flask, render_template, request
 app=Flask(__name__)
 
@@ -27,5 +28,5 @@ def different():
 
 
 if __name__ == "__main__":
-	app.run(port=1115)
+	app.run(port=1121)
 
